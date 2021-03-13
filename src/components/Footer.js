4e55e4bbde1f65dd3f5519fb5eecfd661875c9af
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2021-03-13 11:58:20 +0100

    home done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,54 +8,30 @@ function Footer() {
                     <div className="row">
                         {/* colum 1 */}
                         <div className= "col-md-3 col-sm-6">
-                            <h4>lorem Ipsum</h4>
+                            <h4>Page Map</h4>
                             <ul className="list-unstyled">
-                                <li>Lorem 2d</li>
-                                <li>Lorem 2d</li>
-                                <li>Lorem 2d</li>
-                                <li>Lorem 2d</li>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Products</li>
+                                
 
                             </ul>
                         </div> 
                             {/* colum 2 */}
-                            <div className= "col-md-3 col-sm-6">
-                            <h4>lorem Ipsum</h4>
+                        <div className= "col-md-3 col-sm-6">
+                            <h4>Follow on social media</h4>
                             <ul className="list-unstyled">
-                                <li>Lorem 2d</li>
-                                <li>Lorem 2d</li>
-                                <li>Lorem 2d</li>
-                                <li>Lorem 2d</li>
-
+                                <li>facebook</li>
+                                <li>instagram</li>
+                                <li>Twitter</li>
                             </ul>
-                        </div> 
-                            {/* colum 3 */}
-                            <div className= "col-md-3 col-sm-6">
-                            <h4>lorem Ipsum</h4>
-                            <ul className="list-unstyled">
-                                <li>Lorem 2d</li>
-                                <li>Lorem 2d</li>
-                                <li>Lorem 2d</li>
-                                <li>Lorem 2d</li>
 
-                            </ul>
-                        </div> 
-                            {/* colum 1 */}
-                            <div className= "col-md-3 col-sm-6">
-                            <h4>lorem Ipsum</h4>
-                            <ul className="list-unstyled">
-                                <li>Lorem 2d</li>
-                                <li>Lorem 2d</li>
-                                <li>Lorem 2d</li>
-                                <li>Lorem 2d</li>
-
-                            </ul>
-                        </div> 
-                        <div className="footer-bottom">
-                        <p className="text-xs-center">
-                            &copy; Home Decor - All Rights Reserved
-                        </p>
                         </div>
-
+                        <div className="footer-bottom">
+                            <p className="text-xs-center">
+                                &copy; Home Decor - All Rights Reserved
+                            </p>
+                            </div>
                     </div>
                 </div>
             </div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,52 +1,108 @@
 import React from 'react'
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
-import img5 from "../assets/images/img-05.webp"
-import img11 from "../assets/images/img-11.webp"
-import img10 from "../assets/images/img-01-changed.webp"
+import img2 from "../assets/images/img-02.webp"
+import img3 from "../assets/images/img-03.webp"
+import img4 from "../assets/images/img-04.webp"
+
+
+
 import Button from "react-bootstrap/Button";
 
 
 
 function Products() {
     return (
+    < div className="products">
+        <h2>Products</h2>
         <CardGroup>
         <Card>
           <div>
-          <Card.Img variant="top" src={img5} alt="living-room" />
+          <Card.Img variant="top" src={img2} alt="living-room" />
           </div>
           <Card.Body>
-            <Card.Title>Living Room</Card.Title>
-            <Card.Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </Card.Text>
+            <Card.Title>product 01</Card.Title>
           </Card.Body>
+            <Card.Text>
+                kr: 2500,00
+            </Card.Text>
+          <Button variant="primary">Go to Products</Button>{' '}
+        </Card>
+        <Card>
+          <div>
+          <Card.Img variant="top" src={img3} alt="living-room" />
+          </div>
+          <Card.Body>
+            <Card.Title>product 02</Card.Title>
+          </Card.Body>
+          <Card.Text>
+                kr: 2500,00
+            </Card.Text>
           <Button variant="primary">Go to Products</Button>{' '}
         </Card>
 
         <Card>
-          <Card.Img variant="top"src={img11} alt="chair 2" />
+          <div>
+          <Card.Img variant="top" src={img4} alt="living-room" />
+          </div>
           <Card.Body>
-            <Card.Title>Bedroom</Card.Title>
-            <Card.Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </Card.Text>
+            <Card.Title>product 03</Card.Title>
+           
           </Card.Body>
+          <Card.Text>
+                kr: 2500,00
+            </Card.Text>
           <Button variant="primary">Go to Products</Button>{' '}
         </Card>
         
-        <Card>
-          <Card.Img variant="top" src={img10} alt="chair 3" />
-          <Card.Body>
-            <Card.Title>kitchen</Card.Title>
-            <Card.Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </Card.Text>
-          </Card.Body>
-          <Button variant="primary">Go to products</Button>{' '}
-        </Card>
       </CardGroup>
-    )
+
+      <CardGroup>
+        <Card>
+          <div>
+          <Card.Img variant="top" src={img2} alt="living-room" />
+          </div>
+          <Card.Body>
+            <Card.Title>product 01</Card.Title>
+          </Card.Body>
+          <Card.Text>
+                kr: 2500,00
+            </Card.Text>
+          <Button variant="primary">Go to Products</Button>{' '}
+        </Card>
+        <Card>
+          <div>
+          <Card.Img variant="top" src={img3} alt="living-room" />
+          </div>
+          <Card.Body>
+            <Card.Title>product 02</Card.Title>
+          </Card.Body>
+          <Card.Text>
+                kr: 2500,00
+            </Card.Text>
+          <Button variant="primary">Go to Products</Button>{' '}
+        </Card>
+
+        <Card>
+          <div>
+          <Card.Img variant="top" src={img4} alt="living-room" />
+          </div>
+          <Card.Body>
+            <Card.Title>product 03</Card.Title>
+           
+          </Card.Body>
+          <Card.Text>
+                kr: 2500,00
+            </Card.Text>
+          <Button variant="primary">Go to Products</Button>{' '}
+        </Card>
+
+    
+      </CardGroup>
+    </div>
+  )
+
+
 }
 
 export default Products
