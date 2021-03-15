487e211a0bca5853cec716b8e62fedf5ce85af10
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2021-03-15 15:03:59 +0100

    MA02 new pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./sass/style.scss";
-import Heading from "./components/Heading";
-import NavigationBar from "./components/NavigationBar";
-import HeadingImage  from "./components/HeadingImage";
-import ProductCards from "./components/ProductCards";
-import Testemunials from "./components/Testemunials";
-import FooterPage from "./components/Footer";
-import Products from "./components/Products";
-
+// import Heading from "./components/Heading";
+// import NavigationBar from "./components/NavigationBar";
+// import HeadingImage  from "./components/HeadingImage";
+// import ProductCards from "./components/ProductCards";
+// import Testemunials from "./components/Testemunials";
+// import FooterPage from "./components/Footer";
+// import Products from "./components/Products";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-    return (
+    return (      
       <>
-      <NavigationBar/>
+          <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/contact">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
+      {/* <NavigationBar/>
       <HeadingImage/>
       <div className="container">
           <Heading/>
@@ -20,7 +60,7 @@ function App() {
           <Products/>
           <Testemunials/>
       </div>
-      <FooterPage/>
+      <FooterPage/> */}
         </>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Heading from "../components/Heading";
+import HeadingImage  from "../components/HeadingImage";
+import ProductCards from "../components/ProductCards";
+import Testemunials from "../components/Testemunials";
+import Products from "../components/Products";
 
-export  const Home=() => (
-        <div>
-            <h2>Hello Word</h2>
-            <p>Lorem ipsum</p>
-        </div>
+function Home() {
+    return (
+        <>
+        <HeadingImage/>
+            <div className="container">
+                <Heading/>
+                <ProductCards/>
+                <Products/>
+                <Testemunials/>
+            </div>
+        </>
     )
+}
+    export default Home
